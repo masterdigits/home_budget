c9a0520bf74af5f21448b30b1dc88eed345ada81
Konrad Rulak <kornfix@outlook.com>
2019-12-06 18:45:14 +0100

    Testowanie sesji

--- WindowsFormsApp2/uc_formularz_operacja.cs
+++ WindowsFormsApp2/uc_formularz_operacja.cs
@@ -154,7 +154,12 @@ namespace WindowsFormsApp2
         {
             if (operacjaDoEdycji != null)
             {
+                MessageBox.Show("Usuwam sesję!!!");
                 operacjaDoEdycji.usun_sesje();
+            }
+            foreach(sesja_operacja o in operacjaDoEdycji.sesja_operacja)
+            {
+                MessageBox.Show(o.data_stworzenia.ToString());
             }
             operacjaDoEdycji = null;
             Czysc();
